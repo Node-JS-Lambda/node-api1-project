d3d676c655ee0ab0ea4151dd17383c947914d5d2
fritzgt <tuanis@live.com>
2020-09-24 19:44:01 +0300

    added post method

--- server.js
+++ server.js
@@ -7,8 +7,21 @@ server.use(express.json());
 //Setup routes
 // server.use('/myroute', myComponent);
 
-server.use('/', (req, res) => {
+server.get('/', (req, res) => {
   res.status(200).send('Hello');
 });
+
+server.post('/api/users', (req, res) => {
+  const user = req.body;
+
+  if (user.id && user.name && user.bio) {
+    usersArray.push(user);
+    res.json({ message: 'User created succefully' });
+  } else {
+    res.json({ message: 'User missing required info, try again.' });
+  }
+});
+
+const usersArray = [];
 
 module.exports = server;

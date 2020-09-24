d51bd0004f2bfda757c7cb7e78c8184b1baa03aa
fritzgt <tuanis@live.com>
2020-09-24 19:57:50 +0300

    get methods complete

--- server.js
+++ server.js
@@ -7,10 +7,27 @@ server.use(express.json());
 //Setup routes
 // server.use('/myroute', myComponent);
 
-server.get('/', (req, res) => {
-  res.status(200).send('Hello');
+//Return all users
+server.get('/api/users', (req, res) => {
+  if (usersArray.length != 0) {
+    res.json(usersArray);
+  } else {
+    res.status(404).json({ message: 'No users in database' });
+  }
 });
 
+//Return specific user
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  usersArray.forEach((user) => {
+    if (user.id == id) {
+      return res.json(user);
+    }
+  });
+  res.status(404).json({ message: `User with id: ${id} not found.` });
+});
+
+//Crerate new user
 server.post('/api/users', (req, res) => {
   const user = req.body;
 
@@ -18,7 +35,7 @@ server.post('/api/users', (req, res) => {
     usersArray.push(user);
     res.json({ message: 'User created succefully' });
   } else {
-    res.json({ message: 'User missing required info, try again.' });
+    res.status(404).json({ message: 'User missing required info, try again.' });
   }
 });
 
